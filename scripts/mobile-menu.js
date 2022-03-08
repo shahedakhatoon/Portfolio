36d607db866fe8a25a24b017b5964b7a81bac8f1
Shaheda Khatoon <khanshaheda2013@gmail.com>
2022-03-08 18:33:13 +0530

    Removed body scroll when mobile menu is open

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -3,12 +3,14 @@ const sideNav = document.querySelector('.mobileNav');
 const menuLinks = document.querySelectorAll('.menuMobile');
 const head = document.querySelector('.head');
 const mylogo = document.querySelector('.logo');
+const myhtml = document.querySelector('.scroll')
 
 const openMenu = () => {
   sideNav.classList.add('mobileNavActive');
   menuIcon.classList.add('vanish');
   head.classList.add('blur');
   mylogo.classList.add('blur');
+  myhtml.classList.add('bodyscroll');
 };
 
 const closeMenu = () => {
@@ -16,6 +18,7 @@ const closeMenu = () => {
   head.classList.remove('blur');
   menuIcon.classList.remove('vanish');
   mylogo.classList.remove('blur');
+  myhtml.classList.remove('bodyscroll')
 };
 
 menuIcon.addEventListener('click', openMenu);
