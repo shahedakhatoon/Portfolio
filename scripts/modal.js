002e027b1c416278a60bf347f311a370c3cee418
Shaheda Khatoon <khanshaheda2013@gmail.com>
2022-03-10 20:51:09 +0530

    Added Dynamic Modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,38 +1,22 @@
 //Array
 const data = [
     {
-        card: {
-            mobile: 'card1',
-            desktop: 'card1',
-        },
-        name: {
-            mobile: 'Tonic',
-            desktop: 'Tonic',
-        },
-        job: {
-            mobile: {
+        card: 'card1',
+        imageclass: 'mobileimg',
+        desclass: 'description',
+        name: 'Tonic',
+        job:  {
                 client: 'Canopy',
                 role: 'Back End Dev',
                 year: '2015',
-            },
-            desktop: {
-                client: 'Canopy',
-                role: 'Back End Dev',
-                year: '2015',
-            }
         },
         summary: {
-            mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+            details: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
 
-            desktop: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      
             modaldesc:
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
           },
-        image: {
-            mobile: 'src/SnapshootPortfolio.png',
-            desktop: 'desk/SnapshootPortfolio4.png',
-          }, 
+        image: 'src/SnapshootPortfolio.png',
         technologies: {
             html: 'HTML',
             css: 'CSS',
@@ -42,38 +26,21 @@ const data = [
         sourceLINK: ''
     },
     {
-        card: {
-            mobile: 'card2',
-            desktop: 'card5',
-        },
-        name:  {
-            mobile: 'Multi-Post Stories',
-            desktop: 'Multi-Post Stories',
-        },
+        card: 'card2',
+        imageclass: 'image3',
+        desclass: 'description reverse',
+        name: 'Multi-Post Stories',
         job: {
-            mobile: {
                 client: 'Canopy',
                 role: 'Back End Dev',
                 year: '2015',
-            },
-            desktop: {
-                client: 'FACEBOOK',
-                role: 'Full Stack Dev',
-                year: '2015',
-            }
         },
         summary: {
-            mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-
-            desktop: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-
+            details: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
             modaldesc:
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
           },
-        image: {
-            mobile: 'src/SnapshootPortfolio1.png',
-            desktop: 'desk/SnapshootPortfolio5.png',
-          }, 
+        image: 'src/SnapshootPortfolio1.png', 
         technologies: {
             html: 'HTML',
             css: 'CSS',
@@ -84,38 +51,24 @@ const data = [
         sourceLINK: ''
     },
     {
-        card: {
-            mobile: 'card3',
-            desktop: 'card6',
-        },
-        name: {
-            mobile: 'Tonic',
-            desktop: 'FACEBOOK 360',
-        },
+        card: 'card3',
+        imageclass: '',
+        desclass: 'description',
+        name: 'Tonic',
         job: {
-            mobile: {
                 client: 'Canopy',
                 role: 'Back End Dev',
                 year: '2015',
-            },
-            desktop: {
-                client: 'FACEBOOK',
-                role: 'Full Stack Dev',
-                year: '2015',
-            }
         },
+            
         summary: {
-            mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+            details: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
 
-            desktop: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      
             modaldesc:
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
           },
-        image: {
-            mobile: 'src/SnapshootPortfolio2.png',
-            desktop: 'desk/SnapshootPortfolio6.png',
-          }, 
+        image: 'src/SnapshootPortfolio2.png',
+            
         technologies: {
             html: 'HTML',
             css: 'CSS',
@@ -126,38 +79,22 @@ const data = [
         sourceLINK: ''
     },
     {
-        card: {
-            mobile: 'card4',
-            desktop: 'card7',
-        },
-        name:  {
-            mobile: 'Multi-Post Stories',
-            desktop: 'Uber Navigation',
-        },
+        card: 'card4',
+        imageclass: 'image2',
+        desclass: 'description',
+        name:  'Multi-Post Stories',
         job: {
-            mobile: {
                 client: 'Canopy',
                 role: 'Back End Dev',
                 year: '2015',
             },
-            desktop: {
-                client: 'UBER',
-                role: 'Lead Developer',
-                year: '2018',
-            }
-        },
         summary: {
-            mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+            details: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
 
-            desktop: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      
             modaldesc:
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
           },
-        image: {
-            mobile: 'src/SnapshootPortfolio3.png',
-            desktop: 'desk/SnapshootPortfolio7.png',
-          }, 
+        image: 'src/SnapshootPortfolio3.png',
         technologies: {
             html: 'HTML',
             css: 'CSS',
@@ -167,29 +104,129 @@ const data = [
         livelink: '',
         sourceLINK: ''
     },
+    {
+        card: 'card8',
+        imageclass: 'image',
+        desclass: 'description',
+        name: 'Tonic',
+        job: {
+            client: 'Canopy',
+            role: 'Back End Dev',
+            year: '2015',
+        },
+        summary: {
+            details: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      
+            modaldesc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+          },
+        image: 'desk/SnapshootPortfolio4.png',
+        technologies: {
+            html: 'HTML',
+            css: 'CSS',
+            javascript: 'Javascript',
+          },
+        livelink: '',
+        sourceLINK: ''
+    },
+    {
+        card: 'card5',
+        imageclass: 'imageReverse',
+        desclass: 'descriptionReverse',
+        name: 'Multi-Post Stories',
+        job: {
+            client: 'FACEBOOK',
+            role: 'Full Stack Dev',
+            year: '2015',
+        },
+        summary: {
+            details: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+            modaldesc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        },
+        image: 'desk/SnapshootPortfolio5.png',
+        technologies: {
+            html: 'HTML',
+            css: 'CSS',
+            ruby: 'Ruby on rails',
+            javascript: 'Javascript',
+          },
+        livelink: '',
+        sourceLINK: ''
+    },
+    {
+        card: 'card6',
+        imageclass: 'image 2',
+        desclass: 'description',
+        name: 'FACEBOOK 360',
+        job: {
+            client: 'FACEBOOK',
+            role: 'Full Stack Dev',
+            year: '2015',
+        },
+        summary: {
+            details: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+            modaldesc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        },
+        image: 'desk/SnapshootPortfolio6.png',
+        technologies: {
+            html: 'HTML',
+            css: 'CSS',
+            ruby: 'Ruby on rails',
+            javascript: 'Javascript',
+          },
+        livelink: '',
+        sourceLINK: ''
+    },
+    {
+        card: 'card7',
+        imageclass: 'imageReverse',
+        desclass:'descriptionReverse',
+        name: 'Uber Navigation',
+        job: {
+            client: 'UBER',
+            role: 'Lead Developer',
+            year: '2018',
+        },
+        summary: {
+            details: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+            modaldesc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        },
+        image: 'desk/SnapshootPortfolio7.png',
+        technologies: {
+            html: 'HTML',
+            css: 'CSS',
+            ruby: 'Ruby on rails',
+            javascript: 'Javascript',
+          },
+        livelink: '',
+        sourceLINK: ''
+    }
 ];
+
 
 const works = document.querySelector('#works');
 const modal = document.querySelector('#myModal');
 
 //Dynamic html
 Array.from(works.children).forEach((child, index) => {
-    
     child.innerHTML =
-    `<section class="${data[index].card.mobile}">
+    `<section class="${data[index].card}">
         <ul>
             <li class="project">
-                    <img class="mobileimg" src="${data[index].image.mobile}" alt="Snapshot">
-                    <div class="description">
-                        <h2 class="workstitle">${data[index].name.mobile}</h2>
+                    <img class="${data[index].imageclass}" src="${data[index].image}" alt="Snapshot">
+                    <div class="${data[index].desclass}">
+                        <h2 class="workstitle">${data[index].name}</h2>
                         <div class="info">
-                            <p class="subTitle">${data[index].job.client.mobile}</p>
+                            <p class="subTitle">${data[index].job.client}</p>
                             <ul class="infoContainer">
-                                <li class="role">${data[index].job.role.mobile}</li>
-                                <li class="year">${data[index].job.year.mobile}</li>
+                                <li class="role">${data[index].job.role}</li>
+                                <li class="year">${data[index].job.year}</li>
                             </ul>
                         </div>
-                        <p class="projectDescription">${data[index].summary.mobile}</p>
+                        <p class="projectDescription">${data[index].summary.details}</p>
                         <div class="tags">
                             <ul class="tagsFlex">
                                 <li class="html">${data[index].technologies.html}</li>
@@ -197,36 +234,7 @@ Array.from(works.children).forEach((child, index) => {
                                 <li class="javascript">${data[index].technologies.javascript}</li>
                             </ul>
                         </div>
-                        <button type="button" class="viewProject">
-                            See Project
-                        </button>
-                    </div>
-            </li>
-        </ul>
-    </section>
-    <section class="${data[index].card.desktop}">
-        <ul>
-            <li class="project">
-                    <img class="mobileimg" src="${data[index].image.desktop}" alt="Snapshot">
-                    <div class="description">
-                        <h2 class="workstitle">${data[index].name.desktop}</h2>
-                        <div class="info">
-                            <p class="subTitle">${data[index].job.client.desktop}</p>
-                            <ul class="infoContainer">
-                                <li class="role">${data[index].job.role.desktop}</li>
-                                <li class="year">${data[index].job.year.desktop}</li>
-                            </ul>
-                        </div>
-                        <p class="projectDescription">${data[index].summary.desktop}</p>
-                        <div class="tags">
-                            <ul class="tagsFlex">
-                                <li class="html">${data[index].technologies.html}</li>
-                                <li class="ruby">${data[index].technologies.ruby}</li>
-                                <li class="css">${data[index].technologies.css}</li>
-                                <li class="javascript">${data[index].technologies.javascript}</li>
-                            </ul>
-                        </div>
-                        <button type="button" class="viewProject">
+                        <button type="button" onclick="generateModal()" class="viewProject prj-view-btn">
                             See Project
                         </button>
                     </div>
@@ -235,4 +243,44 @@ Array.from(works.children).forEach((child, index) => {
     </section>`;
 });
 
+function generateModal() {
+    console.log("This if modal");
+}
+
 //Popup Modal
+// Array.from(works.children).forEach((item, index) => {
+//     item.firstElementChild.lastElementChild.lastElementChild.firstElementChild.addEventListener('click', () => {
+//       modal.innerHTML = 
+//       `<section class="modal-content">
+//       <div class="popup-text-group">
+//         <div class="text-group">
+//         <h2 class="workstitle">${data[index].name}</h2>
+//         <div class="info">
+//             <p class="subTitle">${data[index].job.client}</p>
+//             <ul class="infoContainer">
+//                 <li class="role">${data[index].job.role}</li>
+//                 <li class="year">${data[index].job.year}</li>
+//             </ul>
+//         </div>
+//         </div>
+//         <span class="close">&times;</span>
+//       </div>
+//         <li><img src="${data[index].image}" alt="icon" class="img-1-1" /></li>
+//       <div class="popup-bottom">
+//         <p class="popup-text-sm">${data[index].description.portfolio}</p>
+//         <div class="popup-block">
+//           <ul class="tags ${data[index].id}">
+//           </ul>
+//           <div class="popup-actions">
+//             <button class="btn btnImg" id="btn" type="button">See Live
+//               <img src="./assets/btnIcon-1.svg" alt="icon" id="imgIcon" />
+//             </button>
+//             <button class="btn btnImg" id="btn" type="button">See Source
+//               <img src="./assets/cat.svg" alt="icon" id="imgIcon" />
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </section>`;
+
+//     });
