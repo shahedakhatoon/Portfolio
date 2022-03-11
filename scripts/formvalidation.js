5b0bbd76cd59ec858a6613269928db033e01cc0c
Shaheda Khatoon <khanshaheda2013@gmail.com>
2022-03-11 23:25:12 +0530

    Form validation made changes

--- scripts/formvalidation.js
+++ scripts/formvalidation.js
@@ -1,11 +1,10 @@
-const contactForm = document.querySelector('.contactForm');
-const emailImput = document.querySelector('.emailAddress');
-const validationMessage = document.querySelector('.validationMessage');
+const contactForm = document.getElementById('contactForm');
+const emailInput = document.getElementById('email');
+const validationMessage = document.getElementById('validationMessage');
 
 contactForm.addEventListener('submit', (event) => {
-  const stringMessage = emailImput.value;
-  if (/[A-Z]/.test(stringMessage)) {
-    validationMessage.innerHTML = 'Your email is supposed to be lower cased.';
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    validationMessage.innerHTML = 'Your email should be in lowercase';
     event.preventDefault();
   }
 });
